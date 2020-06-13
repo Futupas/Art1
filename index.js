310d41e6e510c95e24e4cd585648b44a6958e8a8
Olexandr Panov <futupas@gmail.com>
2020-06-13 22:07:46 +0300

    simplest working version

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require('express');
+const PORT = process.env.PORT || 5000;
 
 express()
-//   .use(express.static(path.join(__dirname, 'public')))
-//   .set('views', path.join(__dirname, 'views'))
-//   .set('view engine', 'ejs')
-  .get('/', (req, res) => {
+.get('/', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World!');
-    //   res.render('pages/index')
-    })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    res.end('Prozhektor perestroiki');
+})
+.listen(PORT, () => {
+    console.log(`Listening on ${ PORT }`);
+} );
--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World!');
-}).listen(8080);
-
-//"test": "echo \"Error: no test specified\" && exit 1"
