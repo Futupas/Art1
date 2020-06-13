978c96e02089aad67b9d094c5e5199b6597a37d3
Olexandr Panov <futupas@gmail.com>
2020-06-13 21:59:40 +0300

    commit

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+  .get('/', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Hello World!');
+    //   res.render('pages/index')
+    })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
