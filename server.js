b5905ec5994e75d5e257299e79c9b0393e07debd
Olexandr Panov <futupas@gmail.com>
2020-06-13 21:47:19 +0300

    commit

--- server.js
+++ server.js
@@ -4,4 +4,6 @@ var http = require('http');
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end('Hello World!');
-}).listen(80);
+}).listen(8080);
+
+//"test": "echo \"Error: no test specified\" && exit 1"
