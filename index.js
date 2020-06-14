77ca3ce8007650d1accfc8366a81753b4a004b0d
Olexandr Panov <futupas@gmail.com>
2020-06-14 20:26:41 +0300

    get a and b

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ express()
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Prozhektor perestroiki');
 })
+.get('/a', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Prozhektor perestroiki a');
+})
+.get('/b', (req, res) => {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Prozhektor perestroiki b');
+})
 .listen(PORT, () => {
     console.log(`Listening on ${ PORT }`);
 } );
