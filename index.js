601d7c31e4b9ac3eb73b70a25d8f8a814eab1aa4
Olexandr Panov <futupas@gmail.com>
2020-06-14 21:03:21 +0300

    add 2 packages

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 'use strict';
 
-let fs = require('fs');
+import {
+    promises as fs
+} from 'fs';
+import {
+    DOMParser
+} from 'xmldom';
+import * as canvas from 'canvas';
+import fetch from 'node-fetch';
+import Canvg, {
+    presets
+} from 'canvg';
 
-// file is included here:
-eval(fs.readFileSync('tools.js')+'');
+const preset = presets.node({
+    DOMParser,
+    canvas,
+    fetch
+});
 
 
 
@@ -23,6 +36,34 @@ express()
 .get('/b', (req, res) => {
     res.writeHead(200, {'Content-Type': 'text/plain'});
     res.end('Prozhektor perestroiki b');
+})
+.get('/bfff', (req, res) => {
+    const jsdom = require("jsdom");
+    const { JSDOM } = jsdom;
+    const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+// console.log(dom.window.document.querySelector("p").textContent); // "Hello world"
+
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Prozhektor perestroiki '+dom.window.document.querySelector("p").textContent);
+})
+.get('/bdddd', (req, res) => {
+    const svg = fs.readFile(input, 'utf8');
+    const canvas = preset.createCanvas(800, 600);
+    const ctx = canvas.getContext('2d');
+    const v = Canvg.fromString(ctx, svg, preset);
+
+    // Render only first frame, ignoring animations.
+    v.render();
+
+    const png = canvas.toBuffer();
+
+    fs.writeFile(output, png);
+
+
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.end('Prozhektor perestroiki b');
+
 })
 .listen(PORT, () => {
     console.log(`Listening on ${ PORT }`);
